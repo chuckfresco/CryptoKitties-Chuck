5677b243213d4a9d41c84149be69be6ec4f9627e
chuckfresco <chuckfrescoofficial@gmail.com>
2018-01-11 14:26:57 -0800

    Updates-favicon

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces');
 const PORT = process.env.PORT || 5000
+const favicon = require('serve-favicon');
+
+
+
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(favicon(__dirname + '/public/img/favicon.ico/favicon.ico'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
